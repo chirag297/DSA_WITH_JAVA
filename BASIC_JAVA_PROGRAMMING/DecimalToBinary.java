c5c57d33fd592fb8fdf538c645d72d257b5ddcb6
Chirag Solanki <chiragsolanki27012004@gmail.com>
2025-06-10 17:12:40 +0530

    UPDATE program

--- BASIC_JAVA_PROGRAMMING/DecimalToBinary.java
+++ BASIC_JAVA_PROGRAMMING/DecimalToBinary.java
@@ -5,11 +5,12 @@ import java.util.*;
 public class DecimalToBinary{
     public static void main(String args[]){
         int binary=0, rem=0,pow=0;
-        int no;
+        int no,n;
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Any Decimal Number: ");
         no=sc.nextInt();
+        n=no;
         
         while(no >0){
         rem = no % 2;
@@ -17,7 +18,7 @@ public class DecimalToBinary{
         pow++;
         no /= 2;
         }
-        System.out.print(binary);
+        System.out.print("The Binary Number Of Decimal Number("+n+") Is: "binary);
         
     }
 
