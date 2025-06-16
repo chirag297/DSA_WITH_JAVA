cbb37dc90061e1b644b800ff6cecbf9ade7a5e1c
Chirag Solanki <chiragsolanki27012004@gmail.com>
2025-06-16 17:45:39 +0530

    Upload Searching Program

---
+++ JAVA-DSA/Searching/LenearSearch.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LenearSearch{
+    public static int Lenear(int arr[], int value){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == value){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[6];
+        int value;
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Enter Number: ");
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the Number You Want TO Search: ");
+        value = sc.nextInt();
+
+        int index = Lenear(arr,value);
+
+        if(index==-1){
+            System.out.println("Value Not Found In Array! ");
+        }else{
+            System.out.println("Element "+value+"Present In Array At Position "+index);
+        }
+
+    }
+}
