be505192c1b80812d0677853ea45918f1aefcdc9
Chirag Solanki <chiragsolanki27012004@gmail.com>
2025-06-20 22:46:34 +0530

    upload Advance Program of An Array

---
+++ BASIC_JAVA_PROGRAMMING/Arrays/KadansSumOfArray.java
@@ -0,0 +1,36 @@
+/* Program To Find Sum Of Sub Array Using Kadan's Algorithem
+Author:- Chirag Solanki */
+
+import java.util.*;
+
+    public class KadansSumOfArray{
+
+        public static void SumOfArray(int arr[]){
+            int cursum = 0;
+           
+            int maxsum = Integer.MIN_VALUE;
+   
+            
+           for(int i=0;i<arr.length;i++){
+            
+            
+                cursum += arr[i];
+                if(cursum < 0){
+                    cursum = 0;
+                }
+                maxsum = Math.max(cursum, maxsum);
+            }
+            System.out.println(maxsum);
+    }
+        
+        public static void main(String args[]){
+
+            int arr[] = new int[5];
+            for(int i=0;i<arr.length;i++){
+                System.out.print("Enter Number: ");
+                arr[i] = sc.nextInt();
+            }
+            SumOfArray(arr);
+        }
+    
+}
--- BASIC_JAVA_PROGRAMMING/Arrays/PrefixSumOfSubArray.java
+++ BASIC_JAVA_PROGRAMMING/Arrays/PrefixSumOfSubArray.java
@@ -7,29 +7,38 @@ public class PrefixSumOfSubArray{
     public static void SumOfArray(int arr[]){
 
         int prefix[] = new int[arr.length];
-    
+        int cursum = 0;
+        int maxsum=Integer.MIN_VALUE;   
 
-        
-        for(int i = 0;i<arr.length;i++){
+        prefix[0] = arr[0];
+        for(int i = 1;i<prefix.length;i++){
             prefix[i] = prefix[i-1] + arr[i];
         }
         for(int i=0;i<arr.length;i++){
             int start = i;
-
+            cursum = 0;
             for(int j=i;j<arr.length;j++){
                 int end = j;
-            
-
-                System.out.println();
+                cursum = start==0 ? prefix[end] : prefix[end] - prefix[start-1];
             }
-            
+
+            if(maxsum<cursum){
+                maxsum = cursum;
+            }            
 
         }
+
+        System.out.println(maxsum);
         
     }
     public static void main(String args[]){
 
-        int arr[] = {2,4,6,8,10};
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Enter Number: ");
+            arr[i] = sc.nextInt();
+        }
         SumOfArray(arr);
 
     }
